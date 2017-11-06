16c81241a01c028c7c1ac6860b1e1e768c967c03
Dejan Knezevic <dejan.knezevic@sbgenomics.com>
2017-11-06 11:15:55 +0100

    Update library information
    Also replace Markdown README with reStructuredText one for pypi

--- setup.py
+++ setup.py
@@ -10,6 +10,14 @@ if os.path.isfile(VERSION_FILE):
     with io.open(VERSION_FILE, 'r', encoding='utf-8') as f:
         version = f.read()
 
+
+description = long_description = 'Python dependency management helper'
+README_FILE = 'README.rst'
+if os.path.isfile(README_FILE):
+    with io.open(README_FILE, 'r', encoding='utf-8') as f:
+        long_description = f.read()
+
+
 install_requires = [
     'pip>=9.0.1, <10',
     'pipdeptree>=0.10.1, <0.11',
@@ -22,10 +30,28 @@ extras_require = {
 setup(
     name='dante',
     version=version,
-    description='Python dependency management helper',
+    description=description,
+    long_description=long_description,
+    platforms=['Windows', 'POSIX', 'MacOs'],
+    maintainer='Seven Bridges Genomics Inc.',
+    maintainer_email='dejan.knezevic@sbgenomics.com',
+    url='https://github.com/sbg/dante',
+    license='Apache Software License 2.0',
     install_requires=install_requires,
     extras_require=extras_require,
-    packages=find_packages(),
+    packages=find_packages(exclude=['*.tests']),
+    keywords=[
+        'constraint', 'dependency', 'pip',
+        'requirement', 'sbg', 'sevenbridges'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     entry_points={
         'console_scripts': [
             'dante = dante.__main__:main'
